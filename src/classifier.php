77d030e7f00ea9b76a6703ef664f4ff7d295319f
Kostas Bariotis <konmpar@gmail.com>
2014-12-18 21:07:34 +0200

    big refactor, singleton class added

--- src/classifier.php
+++ src/classifier.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Classifier\Classifier;
+namespace Classifier;
 
 /**
  *
@@ -9,7 +9,8 @@ namespace Classifier\Classifier;
  * @author: kbariotis (konmpar at gmail dot com)
  *
  * That's a basic document classification algorithm inspired by Burak Kanber at
- * http://burakkanber.com/blog/machine-learning-naive-bayes-1/( Thanks for the great article dude! ). I basicaly rewrite the code in PHP
+ * http://burakkanber.com/blog/machine-learning-naive-bayes-1/( Thanks for the great article dude! ).
+ * I basicaly rewrite the code in PHP
  * and added some modifications to create a document classification example rather than a language detection system.
  *
  */
@@ -17,184 +18,304 @@ Class Classifier
 {
 
     /**
-     * @var string the input text from the user
-     */
-    var $text;
-
-    /*
-     * @var string the input label from the user
-     */
-    var $label;
-
-    /**
      * @var Spot Spot Object
      */
     var $db;
 
-    /**
-     * @var array the keywords we are going to use
-     */
-    var $words = array();
-
-
-    function __construct($text = '', $label = '')
+    function __construct(\Spot\Locator $spot)
     {
-        $this->text  = $text;
-        $this->label = $label;
+        date_default_timezone_set('Europe/Athens');
 
-        $this->words = $this->parse($text);
-
-        $cfg = new \Spot\Config();
-
-        $cfg->addConnection('sqlite', 'sqlite://../data/database.sqlite');
-
-        $this->db = new \Spot\Locator($cfg);
+        $this->db = $spot;
 
     }
 
-    // this will return if a word is starting with an Uppercase letter, with UTF-8 Support
-    // http://stackoverflow.com/questions/2814880/how-to-check-if-letter-is-upper-or-lower-in-php
-    private function starts_with_upper($str)
+    /**
+     * This will return if a word is starting with an Uppercase letter, with UTF-8 Support
+     *
+     * @see http://stackoverflow.com/questions/2814880/how-to-check-if-letter-is-upper-or-lower-in-php
+     *
+     * @param $str String The string to examine
+     */
+    private function startsWithUppercase($str)
     {
+
         $chr = mb_substr($str, 0, 1, "UTF-8");
 
         // must be first uppercase letter and more than 2 letters
         return mb_strtolower($chr, "UTF-8") != $chr && mb_strlen($str, "UTF-8") > 1;
     }
 
-    // this is our training method, which just parsing the text and push the keywords to DB
-    public function train()
+    /**
+     * This is our training method, that parses the text and push the keywords to DB
+     *
+     * @param $label
+     * @param $text
+     */
+    public function train($label, $text)
     {
 
-        $labelMapper = $this->db->mapper('Classifier\Entity\Label');
-        $label       = $labelMapper->get();
-        $label->name = $this->label;
-        $labelMapper->insert($label);
+        $keywords = $this->parse($text);
 
-        $words = $this->words;
-        for ($i = 0; $i < count($words); $i++) {
-            $wordMapper  = $this->db->mapper('Classifier\Entity\Word');
-            $word        = $wordMapper->get();
-            $word->label = $this->label;
-            $word->name  = $words[ $i ];
-            $wordMapper->insert($word);
+        $labelMapper = $this->db->mapper('Classifier\Entity\Label');
+        $labelMapper->migrate();
+        $labelModel       = $labelMapper->get();
+        $labelModel->name = $label;
+        $labelMapper->insert($labelModel);
+
+        foreach ($keywords as $k) {
+
+            $wordMapper = $this->db->mapper('Classifier\Entity\Word');
+            $wordMapper->migrate();
+            $wordModel        = $wordMapper->get();
+            $wordModel->label = $label;
+            $wordModel->name  = $k;
+            $wordMapper->insert($wordModel);
+
         }
 
     }
 
-    // we need to parse the text from some sort of tokenization
+    /**
+     * We need to parse the text from some sort of tokenization
+     *
+     * We keep only alphanumeric strings that starts with an uppercase
+     *
+     * @param $text
+     *
+     * @return array
+     */
     private function parse($text)
     {
 
-        // strip unwanted characters and create an array with distinct values
         $unwantedChars = array(
             ',', '!', '?', '.', ']', '[', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '/', ':', ';', '<',
             '=', '>', '?', '^', '{', '|', '}', '~', '-', '@', '\', ', '_', '`'
         );
         $str           = str_replace($unwantedChars, ' ', $text);
-        $non1          = explode(" ", $str);
-        $array1        = array_unique($non1);
+        $strToArray    = explode(" ", $str);
+        $finalArray    = array_unique($strToArray);
 
-        // fix array index
-        $array = array_values($array1);
+        $array = array_values($finalArray);
 
-        // trim unwanted spaces
         $result = array_map('trim', $array);
 
-        // length
-        $length = count($array);
-
         $clean = array();
+        foreach ($result as $k)
+            if ($this->startsWithUppercase($k))
+                $clean[ ] = $k;
 
-        for ($i = 0; $i < $length - 1; $i++) {
-            // if we detect a keyword we push it to our global keywords array
-            if ($this->starts_with_upper($result[ $i ])) {
-                $clean[ $i ] = $result[ $i ];
-            }
-        }
-
-        // fix array index and return
         return array_values($clean);
 
     }
 
-    // this is the guessing method, which uses Bayes Theorem to calculate propabilities
-    public function guess()
+    /*
+     * This is the guessing method, which uses Bayes Theorem to calculate probabilities
+     */
+    public function guess($text)
     {
-        // get total documents we have seen so far
-        $this->db->query("SELECT COUNT(label) AS total FROM labels") or die($this->db->error());
-        $totalDocs = $this->db->fetchassoc();
 
-        // holds how many documents we have seen so far for each label
-        $docCounts = array();
-        $this->db->query("SELECT label, COUNT( label ) AS total FROM labels GROUP BY label") or die($this->db->error());
-        while ($row = $this->db->fetcharray()) {
-            $docCounts[ $row[ 'label' ] ] = $row[ 'total' ];
-        }
+        $scores = array();
+        $words  = $this->parse($text);
 
-        // holds how many documents there are that doesn't contain a label
-        $docInverseCounts = array();
-        foreach ($docCounts as $key => $item) {
-            $docInverseCounts[ $key ] = $totalDocs[ 'total' ] - $item;
-        }
+        $labels = $this->getDistinctLabels();
 
-        // fetch all labels
-        $this->db->query("SELECT DISTINCT(label) FROM labels") or die($this->db->error());
-        $labels = array();
-        while ($row = $this->db->fetcharray()) {
-            array_push($labels, $row[ 'label' ]);
-        }
-
-        // for every label loop
-        for ($j = 0; $j < count($labels); $j++) {
-            $label  = $labels[ $j ];
+        foreach ($labels as $label) {
             $logSum = 0;
 
-            // for every word in that label loop
-            for ($i = 0; $i < count($this->words); $i++) {
-                $word = $this->words[ $i ];
+            foreach ($words as $word) {
 
-                // deternine how many times we have seen this word before
-                $this->db->query("SELECT COUNT(word) AS total FROM words WHERE word='$word'") or die($this->db->error());
-                $stemTotalCount = $this->db->fetchassoc();
+                $wordTotalCount = $this->getWordCount($word);
 
-                // if we haven't seen it before, skip it
-                if ($stemTotalCount[ 'total' ] == 0) {
+                if ($wordTotalCount == 0) {
+
                     continue;
+
                 } else {
-                    // probability that this word shows up in a LABEL document
-                    $this->db->query("SELECT COUNT(word) AS total FROM words WHERE word='$word' AND label='$label'") or die($this->db->error());
-                    $wordProbabilityTemp = $this->db->fetchassoc();
-                    $wordProbability     = $wordProbabilityTemp[ 'total' ] / $docCounts[ $label ];
 
-                    // probability that this word shows up in a any other LABEL
-                    $this->db->query("SELECT COUNT(word) AS total FROM words WHERE word='$word' AND label <> '$label'") or die($this->db->error());
-                    $wordInverseProbabilityTemp = $this->db->fetchassoc();
-                    $wordInverseProbability     = $wordInverseProbabilityTemp[ 'total' ] / $docInverseCounts[ $label ];
+                    $wordProbability        = $this->getWordProbabilityWithLabel($word, $label);
+                    $wordInverseProbability = $this->getInverseWordProbabilityWithLabel($word, $label);
 
-                    // HERE IS BAYES THEOREM using the above parameters
-                    // the probability that this document is a particular LABEL given that a particular WORD is in it
+                    /**
+                     * Bayes Theorem using the above parameters
+                     *
+                     * the probability that this document is a particular LABEL
+                     * given that a particular WORD is in it
+                     *
+                     */
                     $wordicity = $wordProbability / ($wordProbability + $wordInverseProbability);
 
-                    // here 0.5 is the weight, higher training data in the db means higher weight
-                    $wordicity = ((10 * 0.5) + ($stemTotalCount[ 'total' ] * $wordicity)) / (10 + $stemTotalCount[ 'total' ]);
+                    /*
+                     * here 0.5 is the weight, higher training data in the db means higher weight
+                     */
+                    $wordicity = ((10 * 0.5) + ($wordTotalCount * $wordicity)) / (10 + $wordTotalCount);
 
-                    // *
                     if ($wordicity == 0)
                         $wordicity = 0.01;
                     else if ($wordicity == 1)
                         $wordicity = 0.99;
                 }
 
-                // logs to avoid "floating point underflow",
+                /**
+                 * logs to avoid "floating point underflow",
+                 */
                 $logSum += (log(1 - $wordicity) - log($wordicity));
             }
 
-            // undo the log function and get back to 0-1 range
+
+            /**
+             * undo the log function and get back to 0-1 range
+             */
             $scores[ $label ] = 1 / (1 + exp($logSum));
         }
 
+
         return $scores;
+    }
+
+
+    /**
+     * Get total documents the system has been trained,
+     * by counting the number of labels (not distinct)
+     *
+     * @return int
+     */
+    public function getTotalDocs()
+    {
+        $labelMapper = $this->db->mapper('Classifier\Entity\Label');
+
+        return count($labelMapper->all());
+    }
+
+    /**
+     * Get how many documents we have seen so far for each label
+     *
+     * @return array
+     */
+    public function getTotalDocsGroupByLabel()
+    {
+        $labelMapper = $this->db->mapper('Classifier\Entity\Label');
+
+        $eachLabelTotal = $labelMapper->query("SELECT name, COUNT( name ) AS total FROM labels GROUP BY name");
+
+        $docCounts = array();
+        foreach ($eachLabelTotal as $r)
+            $docCounts[ $r->name ] = $r->total;
+
+        return $docCounts;
+    }
+
+    public function getDistinctLabels()
+    {
+        $labelMapper = $this->db->mapper('Classifier\Entity\Label');
+
+        $collection = $labelMapper->query("SELECT DISTINCT(name) FROM labels");
+        $labels     = array();
+        foreach ($collection as $r)
+            array_push($labels, $r->name);
+
+        return $labels;
+    }
+
+    /**
+     * Get how many documents there are that does not contain a label
+     * grouped by label
+     *
+     * @return array
+     */
+    public function getInverseTotalDocsGroupByLabel()
+    {
+        $docCounts = $this->getTotalDocsGroupByLabel();
+        $totalDocs = $this->getTotalDocs();
+
+        $docInverseCounts = array();
+        foreach ($docCounts as $key => $item) {
+            $docInverseCounts[ $key ] = $totalDocs - $item;
+        }
+
+        return $docInverseCounts;
+    }
+
+    /**
+     * Get how many times we have seen this word before
+     *
+     * @param $word
+     *
+     * @return int
+     */
+    public function getWordCount($word)
+    {
+        $wordMapper = $this->db->mapper('Classifier\Entity\Word');
+
+        return count(
+            $wordMapper->query("SELECT COUNT(name) AS total FROM words WHERE name= :word",
+                               [
+                                   'word' => $word
+                               ])
+        );
+    }
+
+    /**
+     * Get the probability that this word shows up in a LABEL document
+     *
+     * @param $word
+     * @param $label
+     *
+     * @return float
+     */
+    public function getWordProbabilityWithLabel($word, $label)
+    {
+        $wordMapper = $this->db->mapper('Classifier\Entity\Word');
+
+        $wordProbabilityTemp =
+            $wordMapper->query("SELECT COUNT(name) AS total FROM words WHERE name=:word AND label=:label",
+                               [
+                                   'word'  => $word,
+                                   'label' => $label,
+                               ]);
+
+        $docCounts = $this->getTotalDocsGroupByLabel();
+
+        return $wordProbabilityTemp->first()->total / $docCounts[ $label ];
+    }
+
+    /**
+     * Get the probability that this word shows up in a any other LABEL
+     *
+     * @param $word
+     * @param $label
+     *
+     * @return float
+     */
+    public function getInverseWordProbabilityWithLabel($word, $label)
+    {
+        $wordMapper = $this->db->mapper('Classifier\Entity\Word');
+
+        $wordInverseProbabilityTemp =
+            $wordMapper->query("SELECT COUNT(name) AS total FROM words WHERE name=:word AND label <> :label",
+                               [
+                                   'word'  => $word,
+                                   'label' => $label,
+                               ]);
+
+        $docInverseCounts = $this->getInverseTotalDocsGroupByLabel();
+
+        return $wordInverseProbabilityTemp->first()->total / $docInverseCounts[ $label ];
+    }
+
+    /**
+     * Singleton Pattern
+     *
+     * @return Classifier
+     */
+    public static function getInstance($spot)
+    {
+        static $instance = null;
+        if (null === $instance) {
+            $instance = new static($spot);
+        }
+
+        return $instance;
     }
 
 }
